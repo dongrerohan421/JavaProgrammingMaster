a2b1394e579fc85a0825af12c9fa06b61e6f28f7
dongrerohan421 <dongrerohan421@gmail.com>
2020-08-03 13:40:26 -0400

    create LastDigitCheckerTest

---
+++ src/test/java/com/section5_control_flow_statements/ce19/LastDigitCheckerTest.java
@@ -0,0 +1,38 @@
+package com.section5_control_flow_statements.ce19;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class LastDigitCheckerTest {
+
+    static LastDigitChecker lastDigitChecker;
+
+    @BeforeAll
+    public static void init() {
+        lastDigitChecker = new LastDigitChecker();
+    }
+
+    @SuppressWarnings("static-access")
+    @Test
+    public void hasSameLastDigitWithinRangeTest() {
+        boolean expected = true;
+        boolean actual = lastDigitChecker.hasSameLastDigit(23, 32, 42);
+
+        Assertions.assertEquals(expected, actual);
+
+        expected = true;
+        actual = lastDigitChecker.hasSameLastDigit(41, 22, 71);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @SuppressWarnings("static-access")
+    @Test
+    public void hasSameLastDigitNotWithinRangeTest() {
+        boolean expected = true;
+        boolean actual = lastDigitChecker.hasSameLastDigit(9, 99, 999);
+
+        Assertions.assertNotEquals(expected, actual);
+    }
+}
