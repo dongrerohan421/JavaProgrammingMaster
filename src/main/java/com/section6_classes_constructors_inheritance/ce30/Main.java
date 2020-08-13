4dcb2ba692ef5d88e9f7e4fbd2b2d6b5b1f98592
dongrerohan421 <dongrerohan421@gmail.com>
2020-08-13 14:54:21 -0400

    remove print statements

--- src/main/java/com/section6_classes_constructors_inheritance/ce30/Main.java
+++ src/main/java/com/section6_classes_constructors_inheritance/ce30/Main.java
@@ -8,17 +8,9 @@ public class Main {
         person.setLastName(""); // lastName is set to empty string
         person.setAge(10);
 
-        System.out.println("fullName= " + person.getFullName());
-        System.out.println("teen= " + person.isTeen());
-
         person.setFirstName("John"); // firstName is set to John
         person.setAge(18);
 
-        System.out.println("fullName= " + person.getFullName());
-        System.out.println("teen= " + person.isTeen());
-
         person.setLastName("Smith"); // lastName is set to Smith
-
-        System.out.println("fullName= " + person.getFullName());
     }
 }
