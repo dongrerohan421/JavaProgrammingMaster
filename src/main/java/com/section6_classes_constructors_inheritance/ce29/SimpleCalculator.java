2259b93c66900b4c06e6c26e18a906ce608db254
dongrerohan421 <dongrerohan421@gmail.com>
2020-08-13 13:51:04 -0400

    implementation SimpleCalculator

---
+++ src/main/java/com/section6_classes_constructors_inheritance/ce29/SimpleCalculator.java
@@ -0,0 +1,38 @@
+package com.section6_classes_constructors_inheritance.ce29;
+
+public class SimpleCalculator {
+
+    private double firstNumber;
+    private double secondNumber;
+
+	public double getFirstNumber() {
+		return firstNumber;
+	}
+	public void setFirstNumber(double firstNumber) {
+		this.firstNumber = firstNumber;
+	}
+	public double getSecondNumber() {
+		return secondNumber;
+	}
+	public void setSecondNumber(double secondNumber) {
+		this.secondNumber = secondNumber;
+	}
+    
+    public double getAdditionResult() {
+        return firstNumber + secondNumber;
+    }
+
+    public double getSubtractionResult() {
+        return firstNumber - secondNumber;
+    }
+
+    public double getMultiplicationResult() {
+        return firstNumber * secondNumber;
+    }
+
+    public double getDivisionResult() {
+        if (secondNumber == 0)
+            return 0;
+        return firstNumber / secondNumber;
+    }
+}
