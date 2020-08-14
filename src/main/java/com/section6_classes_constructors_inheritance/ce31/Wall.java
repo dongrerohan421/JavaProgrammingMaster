d84223ad1260ecc480b9ba15b6b083fd5124d1f9
dongrerohan421 <dongrerohan421@gmail.com>
2020-08-14 00:26:28 -0400

    implement Wall

---
+++ src/main/java/com/section6_classes_constructors_inheritance/ce31/Wall.java
@@ -0,0 +1,35 @@
+package com.section6_classes_constructors_inheritance.ce31;
+
+public class Wall {
+    private double width;
+    private double height;
+
+    public Wall() {
+    }
+
+    public Wall(double width, double height) {
+        this.width = (width < 0 ? 0 : width);
+        this.height = (height < 0 ? 0 : height);
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = (width < 0 ? 0 : width);
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = (height < 0 ? 0 : height);
+    }
+
+    public double getArea() {
+        return width * height;
+    }
+
+}
