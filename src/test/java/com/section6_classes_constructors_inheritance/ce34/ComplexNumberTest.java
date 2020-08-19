2bc942c8896a715527096998f4df8e844a6afdbc
dongrerohan421 <dongrerohan421@gmail.com>
2020-08-19 15:30:58 -0400

    ComplexNumber Test cases

---
+++ src/test/java/com/section6_classes_constructors_inheritance/ce34/ComplexNumberTest.java
@@ -0,0 +1,31 @@
+package com.section6_classes_constructors_inheritance.ce34;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class ComplexNumberTest {
+    static ComplexNumber complexNumber;
+
+    @BeforeAll
+    public static void init() {
+        complexNumber = new ComplexNumber();
+    }
+
+    @Test
+    public void testGetReal() {
+        double expected = 3.9;
+        complexNumber = new ComplexNumber(3.9, 1.0);
+        assertEquals(expected, complexNumber.getReal());
+    }
+
+    @Test
+    public void testGetImaginary() {
+        double expected = 2.9;
+        complexNumber = new ComplexNumber(3.9, 2.9);
+        assertEquals(expected, complexNumber.getImaginary());
+    }
+}
