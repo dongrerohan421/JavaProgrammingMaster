920a34e40516ad83839949f45d1a5447f9cb141d
dongrerohan421 <dongrerohan421@gmail.com>
2020-08-19 12:50:42 -0400

    Calculator implementation

---
+++ src/main/java/com/section6_classes_constructors_inheritance/ce33/Calculator.java
@@ -0,0 +1,19 @@
+package com.section6_classes_constructors_inheritance.ce33;
+
+public class Calculator {
+
+    Floor floor;
+    Carpet carpet;
+
+    public Calculator() {
+    }
+
+    public Calculator(Floor floor, Carpet carpet) {
+        this.floor = floor;
+        this.carpet = carpet;
+    }
+
+    public double getTotalCost() {
+        return floor.getArea() * carpet.getCost();
+    }
+}
