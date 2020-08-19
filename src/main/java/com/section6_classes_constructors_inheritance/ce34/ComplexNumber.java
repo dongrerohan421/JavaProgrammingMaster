9205ec9b261b4699d2c61ae6b76002cf983147f9
dongrerohan421 <dongrerohan421@gmail.com>
2020-08-19 15:31:14 -0400

    ComplexNumber implementation

---
+++ src/main/java/com/section6_classes_constructors_inheritance/ce34/ComplexNumber.java
@@ -0,0 +1,43 @@
+package com.section6_classes_constructors_inheritance.ce34;
+
+public class ComplexNumber {
+
+    double real;
+    double imaginary;
+
+    public ComplexNumber() {
+    }
+
+    public ComplexNumber(double real, double imaginary) {
+        this.real = real;
+        this.imaginary = imaginary;
+    }
+
+    public double getReal() {
+        return real;
+    }
+
+    public double getImaginary() {
+        return imaginary;
+    }
+
+    public void add(double real, double imaginary) {
+        this.real += real;
+        this.imaginary += imaginary;
+    }
+
+    public void add(ComplexNumber number) {
+        this.real += number.real;
+        this.imaginary += number.imaginary;
+    }
+
+    public void subtract(double real, double imaginary) {
+        this.real -= real;
+        this.imaginary -= imaginary;
+    }
+
+    public void subtract(ComplexNumber number) {
+        this.real -= number.real;
+        this.imaginary -= number.imaginary;
+    }
+}
