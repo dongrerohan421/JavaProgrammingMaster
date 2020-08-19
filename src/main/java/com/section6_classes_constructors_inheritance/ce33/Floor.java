664a49961c0e5f880747f9bc4195f2bcd8838899
dongrerohan421 <dongrerohan421@gmail.com>
2020-08-19 12:22:06 -0400

    implement Floor

---
+++ src/main/java/com/section6_classes_constructors_inheritance/ce33/Floor.java
@@ -0,0 +1,36 @@
+package com.section6_classes_constructors_inheritance.ce33;
+
+public class Floor {
+
+    double width;
+    double length;
+
+    public Floor() {
+    }
+
+    public Floor(double width, double length) {
+        this.width = (width < 0 ? 0 : width);
+        this.length = (length < 0 ? 0 : length);
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+    public double getArea() {
+        return width * length;
+    }
+
+}
