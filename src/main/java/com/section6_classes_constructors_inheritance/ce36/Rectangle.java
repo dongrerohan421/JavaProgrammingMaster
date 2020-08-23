412a083f4e1db1862cccc90014e2aa452b2dfae3
dongrerohan421 <dongrerohan421@gmail.com>
2020-08-23 15:50:28 -0400

    implement Rectangle

---
+++ src/main/java/com/section6_classes_constructors_inheritance/ce36/Rectangle.java
@@ -0,0 +1,28 @@
+package com.section6_classes_constructors_inheritance.ce36;
+
+public class Rectangle {
+
+    double width;
+    double length;
+
+    public Rectangle(double width, double length) {
+        this.width = (width < 0 ? 0 : width);
+        this.length = (length < 0 ? 0 : length);
+    }
+
+    public Rectangle() {
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public double getArea() {
+        return this.width * this.length;
+    }
+
+}
