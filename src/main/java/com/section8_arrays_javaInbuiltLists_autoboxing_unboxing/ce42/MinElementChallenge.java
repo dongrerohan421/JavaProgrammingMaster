89e6114f3feb2db6bff08e24df67ff53e499743f
dongrerohan421 <dongrerohan421@gmail.com>
2020-09-09 18:07:09 -0400

    input count

--- src/main/java/com/section8_arrays_javaInbuiltLists_autoboxing_unboxing/ce42/MinElementChallenge.java
+++ src/main/java/com/section8_arrays_javaInbuiltLists_autoboxing_unboxing/ce42/MinElementChallenge.java
@@ -1,23 +1,30 @@
 package com.section8_arrays_javainbuiltlists_autoboxing_unboxing.ce42;
 
 import java.util.Arrays;
-import java.util.OptionalInt;
 import java.util.Scanner;
 
 public class MinElementChallenge {
+
+    private static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
 
-        int[] readInteger = readInteger(5);
-        System.out.println(Arrays.toString(readInteger));
+        System.out.print("Enter count: ");
+        int count = scanner.nextInt();
+        scanner.nextLine();
+
+        int[] readInteger = readInteger(count);
+        System.out.println("Entered array is: " + Arrays.toString(readInteger));
 
         System.out.println("Minimum value is: " + findMin(readInteger));
     }
 
     public static int[] readInteger(int count) {
-        Scanner scanner = new Scanner(System.in);
         int[] inputIntegers = new int[count];
         for (int i = 0; i < inputIntegers.length; i++) {
+            System.out.print("Enter a number: ");
             inputIntegers[i] = scanner.nextInt();
+            scanner.nextLine();
         }
         scanner.close();
         return inputIntegers;
